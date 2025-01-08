748830321e157b4ce2c95114426dcb8f4668a2cf
unknown <kottugoroshko232@gmail.com>
2025-01-08 16:51:55 +0200

    added list_tasks detailed_task and creation_task

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from tasks import models, forms
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+class TaskListView(ListView):
+    model = models.Task
+    context_object_name = "tasks"
+    template_name = "tasks/task_list.html"
+
+class TaskDetailView(DetailView):
+    model = models.Task
+    context_object_name = "task"
+    template_name = "tasks/task_details.html"
+
+class TaskCreateView(CreateView):
+    models = models.Task
+    template_name = "tasks/task_form.html"
+    form_class = forms.TaskForm
+    success_url = reverse_lazy("tasks:task-list")
